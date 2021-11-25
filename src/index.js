0c4806f26ec17da0f37e525f2813522ad121992c
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2021-11-25 15:51:37 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import _ from 'lodash'
+//import { NAME as NAME_OF_MY } from './utilities'
+import Tiger from './utilities'
+
+//console.log(NAME_OF_MY);
+
+console.log(Tiger.say());
 
 function component() {
   const element = document.createElement('div');
