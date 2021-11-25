79c69126cfff1493c73a0dc5c93e7c14864befb5
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2021-11-25 15:07:28 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 function component() {
   const element = document.createElement('div');
-  const arry = ['Hello', 'webpack']
+  const arry = ['Hello', 'webpack', '!!']
   element.innerHTML =  _.join(arry, ' ')
   return element;
 }
