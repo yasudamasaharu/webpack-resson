06ba4bf7237ecbfc4526137b635712e697d79388
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2021-11-26 13:31:30 +0900

    implement sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,15 @@ module.exports = {
        test:/\.css$/ ,
        use: [
           'style-loader',
-          'css-loader'          
+          'css-loader'
+       ]
+     },
+     {
+       test:/\.scss$/ ,
+       use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'          
        ]
      },
      {
